91b77b461a22b814ddcaae29507577aaa4ae653c
James Kelleher <thisisjameskelleher@gmail.com>
2019-02-23 14:16:28 +0000

    Server and client in one program for now

--- Server.c
+++ Server.c
@@ -8,11 +8,10 @@
 
 //network socket: two-way communication link between client & server
 //socket is bound to a port
-//server, waits for connection from client by listening to a socket.
+//server, waits for connection from client by listening to a socket
 //client makes connection request knowing hostname and port being listened to
 //server accepts connection & creates a new socket to the same port
-//if connection accepted, socket  created on the client side to communicate
-
+//if connection accepted, socket created on the client side to communicate
 
 void ErrorMessage(const char *msg){
   perror(msg);
@@ -28,9 +27,10 @@ int main(int argc, char *argv[]) {
   char buffer[256]; //read message sent
 
   //struct that stores IP - IP of server and client stored
-  struct sockaddr_in server_addr, client_addr;
+  struct sockaddr_in server_addr;
+  struct sockaddr_in client_addr;
 
-  if(argc < 2) {}
+  if(argc < 2) {
     fprintf(stderr, "Missing port\n");
   }
 
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
   //SOCK_STREAM -> byte stream socket, for TCP
   socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   if(socket_fd < 0) {
-    fprintf(stderr, "Error opening socket\n");
+    fprintf(stderr, "Error: socket could not be opened\n");
   }
  //fills the buffer server_addr with zeros
   bzero((char *)&server_addr, sizeof(server_addr));
@@ -52,11 +52,44 @@ int main(int argc, char *argv[]) {
   server_addr.sin_addr.s_addr = INADDR_ANY;
 
   if(bind(socket_fd, (struct sockaddr *)&server_addr,
-      sizeof(server_addr(addr)) < 0) {
-        perror("Error with connection\n");
-        return;
+          sizeof(server_addr) < 0)) {
+        perror("Error: connection not bound\n");
+        return(-1);
     }
 
   listen(socket_fd, 5);
+
+
+
+
+  //1 give size to client_addr_length to pass on to accept()
+  //2 create another socket with socket descriptor, client address & its size
+  //3 if fails return value < zero and will produce an error
+  //accept() will block  process until a client connects
+  client_addr_length = sizeof(client_addr);
+  new_socket_fd = accept(socket_fd, (struct sockaddr *)&client_addr,
+                         &client_addr_length);
+  if(new_socket_fd < 0) {
+    perror("Error: server couldn't accept client");
+    return(-1);
+  }
+
+  bzero(buffer,256);
+  ret_val = read(new_socket_fd, buffer, 255);
+
+  if(ret_val < 0) {
+    perror("Error: couldn't read socket");
+    return(-1);
+  }
+  printf("Here is the message:%s",buffer);
+
+  ret_val = write(new_socket_fd,"Got your message",16);
+
+  if(ret_val < 0) {
+    perror("Error: couldn't write on socket");
+    return(-1);
+  }
+
+  return 0;
 
 }
